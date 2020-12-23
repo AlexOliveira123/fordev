3f05d1fdad48048d7d60c00c9e08f8dfb39c8edb
alex.oliveira <alex.oliveira@uds.com.br>
2020-12-22 23:49:31 -0300

    move EmailInput to its own file

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../../components/components.dart';
+import 'components/components.dart';
 import 'login_presenter.dart';
 
 class LoginPage extends StatefulWidget {
@@ -43,67 +45,53 @@ class _LoginPageState extends State<LoginPage> {
                 Headline1(text: 'Login'),
                 Padding(
                   padding: EdgeInsets.all(32),
-                  child: Form(
-                    child: Column(
-                      children: [
-                        StreamBuilder<String>(
-                          stream: widget.presenter.emailErrorStream,
-                          builder: (context, snapshot) {
-                            return TextFormField(
-                              decoration: InputDecoration(
-                                labelText: 'Email',
-                                icon: Icon(
-                                  Icons.email,
-                                  color: Theme.of(context).primaryColorLight,
-                                ),
-                                errorText: snapshot.data?.isEmpty == true
-                                    ? null
-                                    : snapshot.data,
-                              ),
-                              onChanged: widget.presenter.validateEmail,
-                              keyboardType: TextInputType.emailAddress,
-                            );
-                          },
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 8.0, bottom: 32),
-                          child: StreamBuilder<String>(
-                              stream: widget.presenter.passwordErrorStream,
-                              builder: (context, snapshot) {
-                                return TextFormField(
-                                  decoration: InputDecoration(
-                                    labelText: 'Senha',
-                                    icon: Icon(
-                                      Icons.lock,
-                                      color:
-                                          Theme.of(context).primaryColorLight,
+                  child: Provider(
+                    create: (_) => widget.presenter,
+                    child: Form(
+                      child: Column(
+                        children: [
+                          EmailInput(),
+                          Padding(
+                            padding: EdgeInsets.only(top: 8.0, bottom: 32),
+                            child: StreamBuilder<String>(
+                                stream: widget.presenter.passwordErrorStream,
+                                builder: (context, snapshot) {
+                                  return TextFormField(
+                                    decoration: InputDecoration(
+                                      labelText: 'Senha',
+                                      icon: Icon(
+                                        Icons.lock,
+                                        color:
+                                            Theme.of(context).primaryColorLight,
+                                      ),
+                                      errorText: snapshot.data?.isEmpty == true
+                                          ? null
+                                          : snapshot.data,
                                     ),
-                                    errorText: snapshot.data?.isEmpty == true
-                                        ? null
-                                        : snapshot.data,
-                                  ),
-                                  obscureText: true,
-                                  onChanged: widget.presenter.validatePassword,
-                                );
-                              }),
-                        ),
-                        StreamBuilder<bool>(
-                          stream: widget.presenter.isFormValidStream,
-                          builder: (context, snapshot) {
-                            return RaisedButton(
-                              onPressed: snapshot.data == true
-                                  ? widget.presenter.auth
-                                  : null,
-                              child: Text('Entrar'.toUpperCase()),
-                            );
-                          },
-                        ),
-                        FlatButton.icon(
-                          onPressed: () {},
-                          icon: Icon(Icons.person),
-                          label: Text('Criar Conta'),
-                        ),
-                      ],
+                                    obscureText: true,
+                                    onChanged:
+                                        widget.presenter.validatePassword,
+                                  );
+                                }),
+                          ),
+                          StreamBuilder<bool>(
+                            stream: widget.presenter.isFormValidStream,
+                            builder: (context, snapshot) {
+                              return RaisedButton(
+                                onPressed: snapshot.data == true
+                                    ? widget.presenter.auth
+                                    : null,
+                                child: Text('Entrar'.toUpperCase()),
+                              );
+                            },
+                          ),
+                          FlatButton.icon(
+                            onPressed: () {},
+                            icon: Icon(Icons.person),
+                            label: Text('Criar Conta'),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 )
