a5db3341ed70b83c48e1aa23ce408acb76aa302a
AlexOliveira123 <allex.c.o@hotmail.com>
2020-12-27 11:44:58 -0300

    ensure StreamLoginPresenter emits correct events on UnexpectedError

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,9 +1,9 @@
 import 'package:faker/faker.dart';
-import 'package:fordev/domain/helpers/domain_error.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
 import 'package:fordev/domain/entities/entities.dart';
+import 'package:fordev/domain/helpers/helpers.dart';
 import 'package:fordev/domain/usecases/usecases.dart';
 
 import 'package:fordev/presentation/presenters/presenters.dart';
@@ -198,6 +198,28 @@ void main() {
 
     sut.mainErrorStream.listen(
       expectAsync1((error) => expect(error, 'Credenciais inválidas.')),
+    );
+
+    await sut.auth();
+  });
+
+  test('Should emit correct events on UnexpectedError', () async {
+    mockAuthenticationError(DomainError.unexpected);
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+
+    expectLater(
+      sut.isLoadingStream,
+      emits(false),
+    );
+
+    sut.mainErrorStream.listen(
+      expectAsync1(
+        (error) => expect(
+          error,
+          'Algo errado aconteceu. Tete novamente em breve.',
+        ),
+      ),
     );
 
     await sut.auth();
