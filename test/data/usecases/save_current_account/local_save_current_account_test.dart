e0b6ce71e29f2433d4d9ef78d58f0b18e4c2f366
AlexOliveira123 <allex.c.o@hotmail.com>
2021-01-02 18:48:40 -0300

    move SaveSecureCacheStorage mock to a helper method

--- test/data/usecases/save_current_account/local_save_current_account_test.dart
+++ test/data/usecases/save_current_account/local_save_current_account_test.dart
@@ -41,6 +41,15 @@ void main() {
     account = AccountEntity(faker.guid.guid());
   });
 
+  void mockError() {
+    when(
+      saveSecureCacheStorage.saveSecure(
+        key: anyNamed('key'),
+        value: anyNamed('value'),
+      ),
+    ).thenThrow(Exception());
+  }
+
   test('Should call SaveSecureCacheStorage with correct values', () async {
     await sut.save(account);
 
@@ -52,12 +61,7 @@ void main() {
 
   test('Should throw UnexpectedError if SaveSecureCacheStorage throws',
       () async {
-    when(
-      saveSecureCacheStorage.saveSecure(
-        key: anyNamed('key'),
-        value: anyNamed('value'),
-      ),
-    ).thenThrow(Exception());
+    mockError();
 
     final future = sut.save(account);
 
